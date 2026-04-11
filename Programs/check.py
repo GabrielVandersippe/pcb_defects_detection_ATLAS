bf8fdaa6fbe40945ec6423c4006c0e4372a454c4
Gabriel Vandersippe <gabriel.vandersippe@etu.minesparis.psl.eu>
2026-04-11 12:06:01 +0200

    Modification des impressions console

--- Programs/check.py
+++ Programs/check.py
@@ -1,4 +1,4 @@
-from Programs.output import print_success, print_error, print_info
+from Programs.output import *
 from Programs.count import extract_serial_number, iref_trim
 from Programs.map import bounding_map_pads_pistes, bounding_map_without_trim, bounding_map_trim
 from Programs.wire_detection import *
@@ -10,101 +10,118 @@ def run_check (path, draw = False) :
 
     ok = True
 
-    img = cv.imread(path)
-    ROI = find_ROI(img)
-    lmask = img[ROI[1][1]:ROI[1][0], ROI[0][0]:ROI[0][1]]
-    rmask = img[ROI[3][1]:ROI[3][0], ROI[2][0]:ROI[2][1]]
+    with console.status(f"[bold blue]{"Préparation à l'analyse de l'image..."}[/bold blue]", spinner = 'dots'):
 
-    y_left, x_left, y_right, x_right = wire_pos(img)
+        img = cv.imread(path)
+        ROI = find_ROI(img)
+        lmask = img[ROI[1][1]:ROI[1][0], ROI[0][0]:ROI[0][1]]
+        rmask = img[ROI[3][1]:ROI[3][0], ROI[2][0]:ROI[2][1]]
 
-    y_left_ROI = y_left - ROI[1][1]
-    x_left_ROI = x_left - ROI[0][0]
+        y_left, x_left, y_right, x_right = wire_pos(img)
 
-    y_right_ROI = y_right - ROI[3][1]
-    x_right_ROI = x_right - ROI[2][0]
+        y_left_ROI = y_left - ROI[1][1]
+        x_left_ROI = x_left - ROI[0][0]
 
+        y_right_ROI = y_right - ROI[3][1]
+        x_right_ROI = x_right - ROI[2][0]
 
-    short_list_left, endpoints_left, labels_left = find_shorts(lmask, 'left', y_left_ROI, x_left_ROI, draw)
-    short_list_right, endpoints_right, labels_right = find_shorts(rmask, 'right', y_right_ROI, x_right_ROI, draw)
-
-    cimg = img.copy()
-
+        cimg = img.copy()
 
     ## ---- CHECKS POUR VERIFIER LE CABLAGE ----
 
     # 1. Vérifier si tous les fils sont présents : 
-    with open("ModulePictures/iref_trim_per_module_v2.json", "r") as f:
-        data = json.load(f)
-        n_expected = expected_wire_number(extract_serial_number(path),data)
-        n_detected = len(y_left) + len(y_right)
-        print("Wires expected : " + str(n_expected))
-        print("Wires detected : " + str(n_detected))
+    with console.status(f"[bold blue]{"Décompte des fils..."}[/bold blue]", spinner = 'dots'):
+        with open("ModulePictures/iref_trim_per_module_v2.json", "r") as f:
+            data = json.load(f)
+            n_expected = expected_wire_number(extract_serial_number(path),data)
+            n_detected = len(y_left) + len(y_right)
+
+    print_info("Décompte des fils effectué.")
 
     if n_expected != n_detected: 
-        print_error("Module incorrect")
-        print_info(f"Nombre des fils : {n_detected}/{n_expected}")
+        print_error(f"Nombre de fils incorrect : {n_detected}/{n_expected}")
         # TODO : dire où il manque des fils
-        
+        # TODO : commande pour forcer à poursuivre les calculs
+
+
+
     else :
-        serial_number = extract_serial_number(path)
-        trim_nb = iref_trim(serial_number, data)
-        bounding_map_trim(trim_nb)
-        wires = bounding_map_pads_pistes() # wires[i] donne le (pad, piste) associés au fil i. 
-        # Format pad : XYYY, X n° du GA, YYY n°du pad
-        # Format piste : XYY, X n° du GA, YY n°de piste
+        print_success(f"    Bon nombre de fils : {n_detected}/{n_expected}") 
+        with console.status(f"[bold blue]{"Recherche des courts-circuits..."}[/bold blue]", spinner = 'dots'):
 
-        nb_shorts = 0
-        tracks = find_tracks(path) # TODO : ajouter la détection des fils dans les bons tracks.
 
-        # 2. Vérifier si des fils qui ne se touchent pas sont bien câblés.
-        for label,endpoint in endpoints_left.items():
+            short_list_left, endpoints_left, labels_left = find_shorts(lmask, 'left', y_left_ROI, x_left_ROI, draw)
+            short_list_right, endpoints_right, labels_right = find_shorts(rmask, 'right', y_right_ROI, x_right_ROI, draw)
 
-            if endpoint != None:
-                (endpoint_location, wire_idx) = endpoint
-                cv.circle(cimg, (endpoint_location[0] + ROI[0][0], endpoint_location[1] + ROI[1][1]), 4, (255, 0, 0), 2)
+            print_info("Décompte des courts-circuits effectué.")
+
+            if len(short_list_left) > 0 : print_error(f"{len(short_list_left)} potentiels courts-circuits à gauche.")
+            else : print_success("Aucun court-circuit à gauche.")
+
+            if len(short_list_right) > 0 : print_error(f"{len(short_list_right)} potentiels courts-circuits à droite.")
+            else : print_success("Aucun court-circuit à droite.")
+
+
+
+        with console.status(f"[bold blue]{"Vérification du câblage pour les fils non court-circuités..."}[/bold blue]", spinner = 'dots'):
+            serial_number = extract_serial_number(path)
+            trim_nb = iref_trim(serial_number, data)
+            bounding_map_trim(trim_nb)
+            wires = bounding_map_pads_pistes() # wires[i] donne le (pad, piste) associés au fil i. 
+            # Format pad : XYYY, X n° du GA, YYY n°du pad
+            # Format piste : XYY, X n° du GA, YY n°de piste
+
+            tracks = find_tracks(path) # TODO : ajouter la détection des fils dans les bons tracks.
+
+            # 2. Vérifier si des fils qui ne se touchent pas sont bien câblés.
+            for label,endpoint in endpoints_left.items():
+
+                if endpoint[0] != None:
+                    (endpoint_location, wire_idx) = endpoint
+                    cv.circle(cimg, (endpoint_location[0] + ROI[0][0], endpoint_location[1] + ROI[1][1]), 4, (255, 0, 0), 2)
+                    
+                    # 3. TODO Vérifier si les fils vont bien au bon endroit
+                    # pad, track = wires[wire_idx[0]]
+                    # in_track = cv.pointPolygonTest(tracks[track%100], endpoint_location)
                 
-                # 3. TODO Vérifier si les fils vont bien au bon endroit
-                pad, track = wires[wire_idx[0]]
-                # in_track = cv.pointPolygonTest(tracks[track%100], endpoint_location)
-            
-            else:
-                nb_shorts += 1
-                _, track = wires[wire_idx[0]]
-                i=0
-                while i < len(wire_idx) and wires[wire_idx[i]][1] == track : 
-                    i+=1
-                # Mettre le fil en rouge, ou en orange s'ils arrivent tous sur une même piste.
-                color = [0, 128*(i==len(wire_idx)), 255]
-                cimg[ROI[1][1]:ROI[1][0], ROI[0][0]:ROI[0][1]][labels_left == label] = color
+                else:
+                    # Coloriage des fils court-circuités.
+                    # _, track = wires[wire_idx[0]]
+                    # i=0
+                    # while i < len(wire_idx) and wires[wire_idx[i]][1] == track : 
+                    #     i+=1
+                    # Mettre le fil en rouge, ou en orange s'ils arrivent tous sur une même piste.
+                    color = [0, 128*(1==len(wire_idx)), 255]
+                    cimg[ROI[1][1]:ROI[1][0], ROI[0][0]:ROI[0][1]][labels_left == label] = color
+                    
                 
-            
 
 
-        for label, endpoint in endpoints_right.items():
-            if endpoint == None:
-                ok = False
-                nb_shorts += 1
-                # Mettre le fil en rouge, TODO ou en orange si OK en termes de pistes d'arivée.
-                cimg[ROI[3][1]:ROI[3][0], ROI[2][0]:ROI[2][1]][labels_right == label] = [0,0,255]
+            for label, endpoint in endpoints_right.items():
+                if endpoint[0] == None:
+                    ok = False
+                    # Mettre le fil en rouge, TODO ou en orange si OK en termes de pistes d'arivée.
+                    cimg[ROI[3][1]:ROI[3][0], ROI[2][0]:ROI[2][1]][labels_right == label] = [0,0,255]
 
-            else :
-                (endpoint_location, wire_idx) = endpoint
-                cv.circle(cimg, (endpoint_location[0] + ROI[2][0], endpoint_location[1] + ROI[3][1]), 4, (255, 0, 0), 2) 
-                
-                # 3. TODO Vérifier si les fils vont bien au bon endroit
+                else :
+                    (endpoint_location, wire_idx) = endpoint
+                    cv.circle(cimg, (endpoint_location[0] + ROI[2][0], endpoint_location[1] + ROI[3][1]), 4, (255, 0, 0), 2) 
+                    
+                    # 3. TODO Vérifier si les fils vont bien au bon endroit
 
-        print(f'{nb_shorts} courts-circuits potentiels détectés.')
-        for short in short_list_left: 
-            cv.circle(cimg, (short[0] + ROI[0][0], short[1] + ROI[1][1]), 4, (0, 255, 0), 2)
-        for short in short_list_right: 
-            cv.circle(cimg, (short[0] + ROI[2][0], short[1] + ROI[3][1]), 4, (0, 255, 0), 2)
 
+
+            for short in short_list_left: 
+                cv.circle(cimg, (short[0] + ROI[0][0], short[1] + ROI[1][1]), 4, (0, 255, 0), 2)
+            for short in short_list_right: 
+                cv.circle(cimg, (short[0] + ROI[2][0], short[1] + ROI[3][1]), 4, (0, 255, 0), 2)
+
+            print_info("Vérification effectuée.")
 
         if ok :
             print_success("Module correctement cablé")
         else :
             print_error("Module incorrect")
-            print_info(f"Bon nombre de fils : {n_detected}/{n_expected}") 
             magnifying_glass(cimg)
             # afficher les zones (1/2/3/4) des fils manquants s'il y en a
             # afficher les zones des fils mal branchés s'il y en a
--- Programs/output.py
+++ Programs/output.py
@@ -3,10 +3,10 @@ from rich.console import Console
 console = Console()
 
 def print_success(msg):
-    console.print(f"[bold green]{msg}[/bold green]")
+    console.print(f"[bold green]{"○" + msg}[/bold green]")
 
 def print_error(msg):
-    console.print(f"[bold red]{msg}[/bold red]")
+    console.print(f"[bold red]{"○" + msg}[/bold red]")
 
 def print_info(msg):
     console.print(f"[bold blue]{msg}[/bold blue]")
